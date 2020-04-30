dfeb3c4ce3d9e5c64f8ce7836dbbee444bdfc477
industriousparadigm <dsgmcosta@gmail.com>
2020-05-01 00:12:47 +0100

    storing user session in JWT inside a cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -8,7 +9,15 @@ import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
 
 const app = express()
+// add rule for express to know it's behind an nginx proxy
+app.set('trust proxy', true)
 app.use(express.json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)

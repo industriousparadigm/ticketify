07a9a831ad04eec8a70e24fa9f4d964da945887c
industriousparadigm <dsgmcosta@gmail.com>
2020-05-02 20:25:51 +0100

    jest added with first test

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('returns 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'diogo@cos.ta',
+      password: 'foobar',
+    })
+    .expect(201)
+})

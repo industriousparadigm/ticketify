7a4a031015434c874e095d1f22963e8523029dcf
industriousparadigm <dsgmcosta@gmail.com>
2020-05-11 15:13:54 +0100

    extracted auth helpers to NPM module

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -5,8 +5,7 @@ import { currentUserRouter } from './routes/current-user'
 import { signupRouter } from './routes/signup'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
+import { errorHandler, NotFoundError } from '@ticketify/common'
 
 const app = express()
 // add rule for express to know it's behind an nginx proxy
